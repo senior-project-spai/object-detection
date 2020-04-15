5a56a745b1d093d195ce9c00178daeb6c0c77f8c
Rawit Panjaroen <check.rawit@gmail.com>
2020-04-15 17:49:02 +0700

    feat: add logger

--- src/app.py
+++ src/app.py
@@ -1,3 +1,13 @@
+# ------------------------------------ log ----------------------------------- #
+import logging
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter(
+    '%(asctime)s - %(name)s - %(message)s'))
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+# ---------------------------------------------------------------------------- #
+
 from kafka import KafkaConsumer, KafkaProducer
 import json
 
@@ -5,11 +15,11 @@ import json
 import s3
 import object_detection
 from config import KAFKA_HOST, KAFKA_PORT, KAFKA_TOPIC_OBJECT_IMAGE
-print("Import complete")
+logger.info("Import complete")
 
 def main():
 
-    print("Setup Kafka Client")
+    logger.info("Setup Kafka Client")
     # Setup Kafka Consumer
     consumer = KafkaConsumer(KAFKA_TOPIC_OBJECT_IMAGE,
                              bootstrap_servers='{}:{}'.format(
@@ -21,23 +31,23 @@ def main():
     producer = KafkaProducer(
         bootstrap_servers='{}:{}'.format(KAFKA_HOST, KAFKA_PORT))
 
-    print("# Ready for consuming #")
+    logger.info("# Ready for consuming #")
 
     for message in consumer:
-        print("Consume Message")
+        logger.info("Consume Message")
 
         # de-serialize
         message_json = json.loads(message.value.decode('utf-8'))
-        print("message_json", message_json)
+        logger.info(message_json)
 
         # Get image from S3
         image_stream = s3.get_file_stream(message_json['object_image_path'])
 
         # detect object in image
-        print("detecting...")
+        logger.info("detecting...")
         detected_image, detections = object_detection.detect(image_stream)
 
-        print(detections)
+        logger.info(detections)
 
 
 # Force to run main()
