9f3bd7a5eaf287d82f3110b3afeadd120b1e0d86
Rawit Panjaroen <check.rawit@gmail.com>
2020-05-01 20:08:43 +0700

    docs: edit detect() docstring

--- src/object_detection.py
+++ src/object_detection.py
@@ -12,10 +12,10 @@ def detect(image_stream):
     Detect object in image stream
 
     Arguments:
-        image_stream {ByteIO} -- image stream
+        ``image_stream`` {ByteIO} -- image stream
 
     Returns:
-        tuple -- numpy image and array of object contain name, percentage_probability and box_points(x1,y1,x2,y2)
+        ``tuple`` -- numpy image and array of object contain ``name``, ``percentage_probability`` and ``box_points``(x1, y1, x2, y2)
     '''
     returned_image, detections = detector.detectObjectsFromImage(input_image=image_stream, input_type="stream",
                                                                  output_type="array", minimum_percentage_probability=30)
